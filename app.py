ffb878cfce423003fb0251c63b08e125a6989d44
Matt Thacker <mthacker01@gmail.com>
2024-12-13 22:42:25 -0500

    Swap pandas as dataframe module

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import polars as pl
+import pandas as pd
 import sqlite3
 
 conn = sqlite3.connect('database.sqlite')
-df = pl.read_database(query='SELECT * FROM players', connection=conn)
+df = pd.read_sql(sql='SELECT * FROM players', con=conn)
 
 st.dataframe(df)
